a85bf9e83e637a440ffa9a37618d54ce81fdd36c
masarani <viral.cby@hotmail.com>
2015-05-10 18:57:56 -0700

    initial commit

---
+++ src/TestCases/SampleClass.java
@@ -0,0 +1,20 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class SampleClass 
+{
+	@Test
+	public void Test1()
+	{
+		
+	}
+	
+	
+	@Test
+	public void Test2()
+	{
+		
+	}
+
+}
